cb1d4808aa2df284deb08f733244a4458183aa61
unknown <tranthiendat220102@gmail.com>
2022-12-17 20:51:13 +0700

    kkk 16

--- src/views/Watch/components/Video/Video.js
+++ src/views/Watch/components/Video/Video.js
@@ -7,12 +7,17 @@ import { AiOutlinePause } from 'react-icons/ai';
 import { MdReplay } from 'react-icons/md';
 import { BiSkipPrevious, BiSkipNext, BiVolumeLow, BiVolumeFull } from 'react-icons/bi';
 
-const videoSrc = require('~/assets/videos/y2mate.com - One Wish  Vu Tử Bối Cover  Vietsub_480p.mp4');
-// const videoSrc = require('~/assets/videos/Mây - Mr.Siro.mp4');
+import axios from 'axios';
+import { apiUrl } from '~/contexts/constants';
+
+// const videoSrc = require('~/assets/videos/y2mate.com - One Wish  Vu Tử Bối Cover  Vietsub_480p.mp4');
+const videoSrc = require('~/assets/videos/Mây - Mr.Siro.mp4');
 
 const cx = classNames.bind(styles);
 
 const Watch = () => {
+   useEffect(() => {}, []);
+
    const watchRef = useRef();
    const videoRef = useRef();
    const watchControlRef = useRef();
@@ -20,11 +25,16 @@ const Watch = () => {
 
    const isEndedRef = useRef(false);
 
+   const videoPreviewRef = useRef();
+
    const [showControl, setShowControl] = useState();
 
    const [play, setPlay] = useState(0);
    const cur_play_pause_Ref = useRef(false);
    const play_pause_Ref = useRef();
+
+   const curImgWrapperRef = useRef();
+   const curImgRef = useRef();
 
    const [showEffectRef, setShowEffectRef] = useState(0);
 
@@ -44,16 +54,6 @@ const Watch = () => {
 
    const curIntervalRef = useRef();
 
-   useEffect(() => {
-      volumeRef.current.onclick = () => {
-         if (volume === 0) {
-            setVolume(volumeCurRef.current.level);
-         } else {
-            setVolume(0);
-         }
-      };
-   }, [volume]);
-
    const handlePlayAndPaus = () => {
       if (videoRef.current.paused) {
          setShowEffectRef(1);
@@ -68,12 +68,33 @@ const Watch = () => {
       }
    };
 
+   var captureImage = function () {
+      var video = videoPreviewRef.current;
+      var canvas = document.createElement('canvas');
+      canvas.width = 192;
+      canvas.height = 108;
+      canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+      //       setTimeout(() => {
+      //          const loadUser = async () => {
+      //             const response = await axios.post(`${apiUrl}/product_details`, {
+      //                productId: `31201662873524613`,
+      //                image: [`${canvas.toDataURL('image/jpeg', 0.4)}`],
+      //             });
+
+      //             console.log(response);
+      //          };
+      //          loadUser();
+      //       }, 100);
+   };
+
    const handleAutoProgress = () => {
       const widthProgress = progressRefRef.current.getBoundingClientRect().width;
       const widthProgressBall = progressBallMain.current.getBoundingClientRect().width / 2;
 
       curIntervalRef.current = setInterval(() => {
          let moveProgress = videoRef.current.currentTime / videoRef.current.duration;
+
          if (moveProgress > 1) {
             moveProgress = 1;
          } else if (moveProgress < 0) {
@@ -117,6 +138,59 @@ const Watch = () => {
       }% - ${widthProgressBall}px))`;
    };
 
+   const handlePreviewVideo = (e) => {
+      const leftProgress = progressRefRef.current.getBoundingClientRect().left;
+      const widthProgress = progressRefRef.current.getBoundingClientRect().width;
+
+      let moveProgress = (e.clientX - leftProgress) / widthProgress;
+
+      videoPreviewRef.current.currentTime = moveProgress * videoPreviewRef.current.duration;
+
+      curImgWrapperRef.current.style.transform = `translateX(calc(${moveProgress * 100}% - 96px))`;
+
+      if (
+         curImgRef.current.getBoundingClientRect().left <=
+         progressRefRef.current.getBoundingClientRect().left
+      ) {
+         curImgWrapperRef.current.style.transform = `translateX(0)`;
+      } else if (
+         curImgRef.current.getBoundingClientRect().right >=
+         progressRefRef.current.getBoundingClientRect().right
+      ) {
+         curImgWrapperRef.current.style.transform = `translateX(calc(${100}% - 192px))`;
+      }
+   };
+
+   useEffect(() => {
+      volumeRef.current.onclick = () => {
+         if (volume === 0) {
+            setVolume(volumeCurRef.current.level);
+         } else {
+            setVolume(0);
+         }
+      };
+   }, [volume]);
+
+   useEffect(() => {
+      videoPreviewRef.current = document.createElement('video');
+      videoPreviewRef.current.src = videoRef.current.src;
+      videoPreviewRef.current.width = '0';
+      videoPreviewRef.current.height = '0';
+      // captureImage();
+
+      let tempImg;
+
+      const loadUser = async () => {
+         const response = await axios.get(`${apiUrl}/product_details`);
+
+         tempImg = response.data.product_details[0].image[0];
+
+         console.log(tempImg);
+         curImgRef.current.style.backgroundImage = `url('${tempImg}`;
+      };
+      loadUser();
+   }, []);
+
    useEffect(() => {
       let uniqueTime;
 
@@ -136,7 +210,7 @@ const Watch = () => {
       }
 
       watchRef.current.onmousemove = () => {
-         if (play === 1) {
+         if (play === 1 && !dragRef.current) {
             setShowControl(1);
          } else setShowControl(2);
       };
@@ -168,9 +242,9 @@ const Watch = () => {
       }
 
       videoRef.current.oncanplay = (e) => {
-         // videoRef.current.muted = true;
-         // videoRef.current.autoplay = true;
-         // setPlay(1);
+         videoRef.current.muted = true;
+         videoRef.current.autoplay = true;
+         setPlay(1);
       };
 
       videoRef.current.onplay = () => {
@@ -217,11 +291,17 @@ const Watch = () => {
    }, [play]);
 
    useEffect(() => {
+      progressRefRef.current.onmousemove = (e) => {
+         handlePreviewVideo(e);
+      };
+
       progressRefRef.current.onmouseover = (e) => {
          mainProgressfRef.current.style.transform = 'scaleY(1)';
          progressBall.current.style.visibility = 'visible';
          progressBallMain.current.style.height = '14px';
          progressBallMain.current.style.width = '14px';
+
+         curImgWrapperRef.current.style.display = 'block';
       };
 
       progressRefRef.current.onmouseleave = (e) => {
@@ -230,11 +310,16 @@ const Watch = () => {
             progressBall.current.style.visibility = 'hidden';
             progressBallMain.current.style.height = '2px';
             progressBallMain.current.style.width = '2px';
+
+            curImgWrapperRef.current.style.display = 'none';
          }
       };
 
       progressRefRef.current.onmousedown = (e) => {
          dragRef.current = true;
+
+         curImgWrapperRef.current.style.display = 'block';
+
          clearTimeout(curIntervalRef.current);
 
          progressCurrent.current.style.transition = `none`;
@@ -256,6 +341,8 @@ const Watch = () => {
 
       document.onmouseup = (e) => {
          if (dragRef.current) {
+            curImgWrapperRef.current.style.display = 'none';
+
             setShowControl(1);
 
             if (cur_play_pause_Ref.current || isEndedRef.current) {
@@ -289,6 +376,9 @@ const Watch = () => {
                   </div>
                   <div ref={progressBall} className={cx('progress-ball__wrapper')}>
                      <div ref={progressBallMain} className={cx('progress-ball')}></div>
+                  </div>
+                  <div ref={curImgWrapperRef} className={cx('wrapper__progress-img')}>
+                     <div ref={curImgRef} className={cx('progress-img')}></div>
                   </div>
                </div>
                <div className={cx('icon')}>
